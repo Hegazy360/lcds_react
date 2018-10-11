d11dfc6c3cc35a4666302ecdc0a01a743c494100
Mohamed Hegazy <m.hegazy94@hotmail.com>
2018-10-11 18:03:24 +0200

    First connection using axios to remote api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
+import axios from 'axios'
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('https://react-rails-staging.herokuapp.com/api/v1/customers')
+    .then(response => {
+      console.log(response.data)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Application Loaded
       </div>
     );
   }
